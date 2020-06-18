0cea0a718c4c528585922e43b9f3410e64df3d4a
olbed <mr.oo.leg@gmail.com>
2020-06-18 18:01:27 +0300

    messages

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
 
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-from .forms import UserRegistrationForm
+from .forms import UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 @login_required
@@ -11,14 +15,36 @@ def dashboard(request):
 
 def register(request):
     if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
+        user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
+            new_user = user_form.save()
+            Profile.objects.create(user=new_user)
+            login(request, new_user)
+            messages.success(request, 'Account registered successfully')
             return redirect('dashboard')
     else:
         user_form = UserRegistrationForm()
 
     ctx = dict(user_form=user_form)
     return render(request, 'account/register.html', ctx)
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+            return redirect('dashboard')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    ctx = dict(user_form=user_form, profile_form=profile_form)
+    return render(request, 'account/edit.html', ctx)
